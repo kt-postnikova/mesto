4c3aed70b96580a9ed15f73e09ade9cb1020ecdd
Kate Postnikova <kt.postnikova@yandex.ru>
2021-01-13 12:16:28 +0300

    feat: add array of cards

--- scripts/index.js
+++ scripts/index.js
@@ -9,6 +9,34 @@ let inputName = container.querySelector('.popup__input_type_name');
 let inputJob = container.querySelector('.popup__input_type_job');
 
 
+const initialCards = [
+    {
+      name: 'Архыз',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+      name: 'Челябинская область',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+      name: 'Иваново',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+      name: 'Камчатка',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+      name: 'Холмогорский район',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+      name: 'Байкал',
+      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+  ];
+
+
 function openEditBtn() {
     inputName.value = profileName.innerText;
     inputJob.value = profileJob.innerText;
