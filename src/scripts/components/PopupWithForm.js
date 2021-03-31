d55e58c70fac29993c1df28f3c8ca05f5b487785
kt-postnikova <kt.postnikova@yandex.ru>
2021-03-31 17:03:28 +0300

    some fixes

--- src/scripts/components/PopupWithForm.js
+++ src/scripts/components/PopupWithForm.js
@@ -9,7 +9,7 @@ export default class PopupWithForm extends Popup {
 
     _getInputValues() {
         this.inputList = Array.from(this.form.querySelectorAll('.form__input'));
-        const inputInfo = {}; // объект с name и about
+        const inputInfo = {};
 
         this.inputList.forEach((input) => {
             inputInfo[input.name] = input.value
