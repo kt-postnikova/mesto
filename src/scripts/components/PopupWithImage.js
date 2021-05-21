372019083c9752dfeedd25fd90bd2dffd1189329
kt-postnikova <kt.postnikova@yandex.ru>
2021-05-21 17:02:57 +0300

    fixed class dependence

--- src/scripts/components/PopupWithImage.js
+++ src/scripts/components/PopupWithImage.js
@@ -1,14 +1,17 @@
-import { popupCaption, popupImage } from "../utils/constants.js";
 import Popup from "./Popup.js";
 
 export default class PopupWithImage extends Popup {
     constructor(popupSelector) {
         super(popupSelector);
+
+        this.popupCaption = this.popupSelector.querySelector('.popup__caption');
+        this.popupImage = this.popupSelector.querySelector('.popup__image');
     }
 
     open(cardData) {
         super.open();
-        popupImage.src = cardData.link;
-        popupCaption.textContent = cardData.name;
+        this.popupImage.src = cardData.link;
+        this.popupImage.alt = cardData.name;
+        this.popupCaption.textContent = cardData.name;
     }
 }
